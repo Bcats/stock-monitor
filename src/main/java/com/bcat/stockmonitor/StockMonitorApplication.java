d1b59d74a7f9cceb57b9ab6724713990c688f7d7
Bcat <205672513@qq.com>
2020-04-11 22:51:08 +0800

    first commit

---
+++ src/main/java/com/bcat/stockmonitor/StockMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.bcat.stockmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMonitorApplication.class, args);
+    }
+
+}
